d987a771ccebe7730d5f74b99d96c9943e960533
Kaique Bosco <kaique.bosco@hotmail.com>
2023-09-27 11:38:43 -0300

    page Categories + routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import Header from './components/Header'
 import { GlobalCSS } from './styles'
-import Home from './pages/Home'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  }
-])
+import Router from './routes'
 
 function App() {
   return (
     <>
-      <GlobalCSS />
-      <div className="container">
-        <Header />
-      </div>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <GlobalCSS />
+        <div className="container">
+          <Header />
+        </div>
+        <Router />
+      </BrowserRouter>
     </>
   )
 }
